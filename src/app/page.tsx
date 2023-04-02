e8647b281f51a11d555e48b5b0b2dd6319c61cf9
husanbek123 <husanbek555@gmail.com>
2023-04-02 17:05:56 +0500

    Husna123123123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
-import { font } from '@/components/font'
+import { font } from "@/components/font";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-     
+      <h2 className={font.className}>Hello world</h2>
     </main>
-  )
+  );
 }
--- src/components/font.ts
+++ src/components/font.ts
@@ -1,2 +1,5 @@
-import { Oswald } from 'next/font/google'
-export const font = Oswald({ subsets: ['latin']  , weight : ['300','400','500'] })
+import { Manrope, Oswald } from "next/font/google";
+export const font = Manrope({
+  subsets: ["latin"],
+  weight: ["300", "400", "500"],
+});
