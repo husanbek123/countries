63400a6f24502574f2178af9571d022ccb58fe72
NurjahonErgashev <lobarqochqorova18@gmail.com>
2023-04-02 17:16:19 +0500

    nmadree

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,47 @@ import { api } from "../utils/axios";
 import { font } from "@/components/font";
 import { useLocalStore } from "@/utils/store/store";
 import { useEffect, useState } from "react";
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Card, Grid, Row, Text, Input, Button } from "@nextui-org/react";
 import { Loader } from "@/components/loader";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   const [datas, setDatas] = useState([]);
   const theme = useLocalStore((state) => state.theme);
   console.log(theme);
   useEffect(() => {
     api.get("/all").then((res) => setDatas(res?.data));
   }, []);
-
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.settings}></div>
+        <div className={styles.settings}>
+          <div className={styles.search}>
+            {datas.length && (
+              <Grid
+                css={{
+                  display: "flex",
+                  gap: "20px",
+                }}
+              >
+                <Input bordered labelPlaceholder="Default" style={font.style} color="primary" />
+                <Button shadow color="primary" auto>
+                  Primary
+                </Button>
+              </Grid>
+            )}
+          </div>
+        </div>
         <div className={styles.wrapper}>
           {datas.length ? (
             <Grid.Container gap={2} justify="flex-start">
               {datas?.map((item, index) => (
-                <Grid xs={6} sm={3} key={index}>
+                <Grid
+                  xs={6}
+                  sm={3}
+                  key={index}
+                  onClick={() => router.push(`/${item?.name?.official}`)}
+                >
                   <Card isPressable>
                     <Card.Body css={{ p: 0 }}>
                       <Card.Image
