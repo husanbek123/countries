91fb929fe40ae89630fad8320a8398f2c16b1c29
husanbek123 <husanbek555@gmail.com>
2023-04-02 17:23:36 +0500

    nasjdnlaksd

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -3,14 +3,7 @@ import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import { api } from "../../utils/axios";
 
-// async function getData() {
-//   let path = usePathname();
-//   console.log(path);
 
-//   let res = await api.get(`/name${path}`);
-//   let data = await res.data;
-//   return data;
-// }
 
 const Country = () => {
   let path = usePathname();
@@ -20,16 +13,3 @@ const Country = () => {
   return <div>{data?.name?.common}</div>;
 };
 export default Country;
-
-// export async function getServerSideProps() {
-//   let path = usePathname();
-
-//   let res = await api.get(`/${path.split("/")}`);
-//   let data = await res.data;
-
-//   return {
-//     props: {
-//       data: data,
-//     },
-//   };
-// }
